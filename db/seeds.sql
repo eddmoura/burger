ac13c5ec6e4579320ebb60b0f246c8f0ee7731e4
[eddmoura] <edd.alca@gmail.com>
2017-07-03 15:05:53 -0400

    update1, still working on it

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Turkey Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Veggie Burger', FALSE);
+
+INSERT INTO burgers (burger_name, devoured) 
+VALUES ('Double Chesseburger', FALSE);
